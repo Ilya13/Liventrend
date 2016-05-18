a1fb8a407bf2711d8aa45c0ec3e12e1e513c6dd9
bugsb <bugsb@ILYA-NOTEBOOK>
2016-05-19 00:55:55 +0500

    1

--- Liventrend/src/ru/vertiprakhov/lcl/client/Liventrend.java
+++ Liventrend/src/ru/vertiprakhov/lcl/client/Liventrend.java
@@ -2,6 +2,7 @@ package ru.vertiprakhov.lcl.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
 
 import ru.vertiprakhov.lcl.client.ui.CrazyTextBox;
 
@@ -13,7 +14,8 @@ public class Liventrend implements EntryPoint {
 	public void onModuleLoad() {
 		final CrazyTextBox nameField = new CrazyTextBox();
 
-		RootPanel.get("nameFieldContainer").add(nameField);
+		RootPanel.get("textBoxContainer").add(new TextBox());
+		RootPanel.get("crazyTextBoxContainer").add(nameField);
 		nameField.setFocus(true);
 	}
 }
--- Liventrend/src/ru/vertiprakhov/lcl/client/ui/CrazyTextBox.java
+++ Liventrend/src/ru/vertiprakhov/lcl/client/ui/CrazyTextBox.java
@@ -3,6 +3,7 @@ package ru.vertiprakhov.lcl.client.ui;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 
 import com.google.gwt.canvas.client.Canvas;
 import com.google.gwt.canvas.dom.client.Context;
@@ -31,18 +32,14 @@ public class CrazyTextBox extends TextBoxBase {
 	private Canvas canvas;
 	private Context2d context;
 
-	private int canvasWidth = 168;
-	private int canvasHeight = 15;
+	private int inputWidth = 168;
+	private int inputHeight = 15;
 	private int fontSize = 13;
-	private int paddingLeft;
-	private int paddingRight;
-	private int paddingTop;
-	private int paddingBottom;
 	
 	private String value = "";
 	private TextRange range = new TextRange();
 	
-	private List<TextPoint> points = new ArrayList<>();
+	private LinkedList<TextPoint> points = new LinkedList<>();
 	
 	public CrazyTextBox() {
 		this(Document.get().createDivElement(), "gwt-TextBox lcl-CrazyTextBox");
@@ -69,14 +66,10 @@ public class CrazyTextBox extends TextBoxBase {
 
 		String fontFamily = getComputedStyleProperty(getElement(), "font-family");
 		String fontSize = getComputedStyleProperty(getElement(), "font-size");
-		setPaddingLeft(getComputedStyleProperty(getElement(), "padding-left"));
-		setPaddingRight(getComputedStyleProperty(getElement(), "padding-right"));
-		setPaddingTop(getComputedStyleProperty(getElement(), "padding-top"));
-		setPaddingBottom(getComputedStyleProperty(getElement(), "padding-bottom"));
 		setFontSize(fontSize);
 		
-		canvas.setCoordinateSpaceWidth(canvasWidth);
-		canvas.setCoordinateSpaceHeight(canvasHeight);
+		canvas.setCoordinateSpaceWidth(inputWidth);
+		canvas.setCoordinateSpaceHeight(inputHeight);
 		
 		context.setFont(fontSize + " " + fontFamily);
 		context.save();
@@ -107,96 +100,137 @@ public class CrazyTextBox extends TextBoxBase {
 	}
 
 	private void addChar(char charCode) {
-		TextPoint point = new TextPoint(charCode, context.measureText(String.valueOf(charCode)).getWidth());
-		range.width += point.getWidth();
-		int drawLeft = 0;
-		boolean clearAll = false;
-		if(range.width > canvasWidth){
-			if (range.curret == points.size()){
-				do {
-					range.width -= points.get(range.left).getWidth();
-					range.left++;
-				} while (range.width > canvasWidth);
-				drawLeft = range.left;
-				clearAll = true;
+		TextPoint point = new TextPoint(charCode, context.measureText(String.valueOf(charCode)).getWidth());		
+		if (range.curret == points.size()){
+			points.addLast(point);
+			range.right++;
+			if(range.width + point.getWidth() <= inputWidth){
+				drawWithoutOffset(point);
 			} else {
+				drawWithLeftOffset(point);
 			}
 		} else {
-			drawLeft = range.right;
+			points.add(range.curret, point);
+			drawWithRightOffset();
 		}
-		points.add(range.curret, point);
-		range.right++;
 		range.curret++;
-
-		double position = 0;
-		if (clearAll){
-			context.clearRect(0, 0, canvasWidth, canvasHeight);
+		range.width += point.getWidth();
+	}
+	
+	/**
+	 * Draw character right of the text without offset other characters.
+	 */
+	private void drawWithoutOffset(TextPoint point) {
+		if ((points.size() & 1) == 0){
+			drawCrazyChar(point, range.width);
 		} else {
-			position = clearRange(drawLeft, range.right);
+			context.fillText(String.valueOf(point.getChr()), range.width, fontSize-1);
 		}
+	}
+	
+	/**
+	 * Draw character when curret inside the text.
+	 */
+	private void drawWithRightOffset() {
+		// TODO Auto-generated method stub
 		
-		drawRange(position, drawLeft, range.right);
 	}
 
-	private double clearRange(int left, int right) {
-		double position = 0;
-		for (int i = 0; i < left; i++) {
-			position += points.get(i).getWidth();
+	/**
+	 * Draw character when text does not fit input.
+	 */
+	private void drawWithLeftOffset(TextPoint point) {
+		ListIterator<TextPoint> iter = points.listIterator(range.left);
+		TextPoint leftPoint = iter.next();
+		while (range.width + point.getWidth() - inputWidth > leftPoint.getWidth()){
+			range.width -= leftPoint.getWidth();
+			leftPoint = iter.next();
+			range.left++;
 		}
-		context.clearRect(position, 0, canvasWidth, canvasHeight);
-		return position;
+		context.clearRect(0, 0, inputWidth, inputHeight);
+		drawRange(inputWidth - range.width - point.getWidth(), range.left, range.right);
+	}
+
+	/**
+	 * Draw flip character.
+	 * @param point the character for drawing
+	 * @param x the x coordinate of the character position
+	 */
+	private void drawCrazyChar(TextPoint point, double x){
+		double y = 0;
+		if (!Character.isLowerCase(point.getChr())){
+			context.setTextBaseline(TextBaseline.BOTTOM);
+		} else {
+			y -= fontSize/2;
+		}
+		context.scale(1, -1);
+		context.fillText(String.valueOf(point.getChr()), x, y+1);
+		context.scale(1, -1);
+		context.setTextBaseline(TextBaseline.ALPHABETIC);
 	}
 	
 	private void drawRange(double position, int left, int right) {
-		for (int i = left; i < right; i++) {
-			TextPoint point = points.get(i);
+		int i = left;
+		ListIterator<TextPoint> iter = points.listIterator(i);
+		while (i < right){
+			TextPoint point = iter.next();
 			if ((i & 1) != 0){
-				double y = -paddingTop;
-				if (!Character.isLowerCase(point.getChr())){
-					context.setTextBaseline(TextBaseline.BOTTOM);
-				} else {
-					y -= fontSize/2;
-				}
-				context.scale(1, -1);
-				context.fillText(String.valueOf(point.getChr()), position, y);
-				context.scale(1, -1);
-				context.setTextBaseline(TextBaseline.ALPHABETIC);
+				drawCrazyChar(point, position);
 			} else {
-				context.fillText(String.valueOf(point.getChr()), position, fontSize + paddingTop);
+				context.fillText(String.valueOf(point.getChr()), position, fontSize-1);
 			}
 			position += point.getWidth();
+			i++;
 		}
 	}
- 
+	
 	private void removeChar() {
-		
+		if(range.width > inputWidth){
+			if (range.curret == points.size()){
+				TextPoint point = points.getLast();
+				range.width -= point.getWidth();
+				range.curret--;
+				range.right--;
+				points.removeLast();
+				removeWithLeftOffset();
+			}
+		} else {
+			if (range.curret == points.size()){
+				TextPoint point = points.getLast();
+				range.width -= point.getWidth();
+				range.curret--;
+				range.right--;
+				points.removeLast();
+				removeWithoutOffset();
+			}
+		}
 	}
 	
+	private void removeWithLeftOffset(TextPoint point) {
+		ListIterator<TextPoint> iter = points.listIterator(range.left);
+		TextPoint leftPoint = iter.next();
+		while (range.width + point.getWidth() - inputWidth < leftPoint.getWidth()){
+			range.width -= leftPoint.getWidth();
+			leftPoint = iter.next();
+			range.left++;
+		}
+		context.clearRect(0, 0, inputWidth, inputHeight);
+		drawRange(inputWidth - range.width - point.getWidth(), range.left, range.right);
+	}
+
+	private void removeWithoutOffset() {
+		ListIterator<TextPoint> iter = points.listIterator();
+		double position = 0;
+		while(iter.hasNext()){
+			position += iter.next().getWidth();
+		}
+		
+		context.clearRect(position, 0, inputWidth, inputHeight);
+	}
+
 	private void setFontSize(String fontSize){
 		this.fontSize = sizeToInt(fontSize);
-		updatHeight();
-	}
-
-	public void setPaddingLeft(String paddingLeft) {
-		this.paddingLeft = sizeToInt(paddingLeft);
-	}
-
-	public void setPaddingRight(String paddingRight) {
-		this.paddingRight = sizeToInt(paddingRight);
-	}
-
-	public void setPaddingTop(String paddingTop) {
-		this.paddingTop = sizeToInt(paddingTop);
-		updatHeight();
-	}
-
-	public void setPaddingBottom(String paddingBottom) {
-		this.paddingBottom = sizeToInt(paddingBottom);
-		updatHeight();
-	}
-	
-	private void updatHeight(){
-		canvasHeight = this.fontSize + this.paddingTop + this.paddingBottom;
+		this.inputHeight = this.fontSize + 2;
 	}
 	
 	private static int sizeToInt(String size){
